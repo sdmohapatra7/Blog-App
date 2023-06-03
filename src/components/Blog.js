ab9edc4d21b6fba6066cd4d1c0b638456ce3c10f
Shaktidhar <sdmohapatra7@gmail.com>
2023-06-03 20:50:39 +0530

    used useRef And useEffect focus and page title

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 
 export default function Blog() {
 
@@ -6,18 +6,36 @@ export default function Blog() {
     // const[content,setContent]=useState('');
     const[formData,setFormData]=useState({title:'',content:''})
     const[blogs,setBlogs]=useState([]);
+    const titleRef = useRef(null);
+
+    useEffect(()=>{
+        titleRef.current.focus()
+    },[]);
+
+    useEffect(()=>{
+        if(blogs.length && blogs[0].title){
+            document.title = blogs[0].title;
+        }else{
+            document.title = 'No Blogs!'
+        }
+    },[blogs]);
 
     const handleSubmit = (e)=>{
         e.preventDefault();
+        
         setBlogs([{title:formData.title,
                 content:formData.content},...blogs]);
         // setTitle('');
         // setContent('');
         setFormData({title:'',content:''});
+        titleRef.current.focus();
     }
     const handleDelete = (index)=>{
         let restBlog = blogs.filter((blog,i)=>i!==index);
         setBlogs(restBlog);
+    }
+    const handleEdit = ()=>{
+
     }
     
     return (
@@ -27,11 +45,11 @@ export default function Blog() {
             <form onSubmit={handleSubmit}>
                 {/* Row component to create a row for first input field */}
                 <Row label='Title'>
-                    <input className='input' placeholder="Enter the Title of the Blog here.." value={formData.title} onChange={(e)=>setFormData({title:e.target.value,content:formData.content})}/>
+                    <input className='input' placeholder="Enter the Title of the Blog here.." value={formData.title} ref={titleRef} onChange={(e)=>setFormData({title:e.target.value,content:formData.content})}/>
                 </Row>
                 {/* Row component to create a row for Text area field */}
                 <Row label='Content'>
-                    <textarea className='input content' placeholder="Content of the Blog goes here.." value={formData.content} onChange={(e)=>setFormData({title:formData.title,content:e.target.value})}/>
+                    <textarea className='input content' placeholder="Content of the Blog goes here.." required value={formData.content} onChange={(e)=>setFormData({title:formData.title,content:e.target.value})}/>
                 </Row>
                 <button className = "btn">ADD</button>
             </form>
@@ -43,6 +61,7 @@ export default function Blog() {
                 <h3>{blog.title}</h3>
                 <p>{blog.content}</p>
                 <div className='blog-btn'>
+                    <button className='btn edit' onClick={()=>handleEdit(index)}>Edit</button>
                     <button className='btn remove' onClick={()=>handleDelete(index)}>Delete</button>
                 </div>
             </div>
