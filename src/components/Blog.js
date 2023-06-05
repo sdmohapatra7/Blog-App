9b275687056831a03226fbf1c2faea7e7bfaa58f
Shaktidhar <sdmohapatra7@gmail.com>
2023-06-05 21:38:10 +0530

    add search and edit feacture

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,97 +1,111 @@
-import React,{useState,useRef,useEffect,useReducer} from 'react'
+import React, { useState, useRef, useEffect, useReducer } from 'react'
 
-function blogsReducer(state,action){
-    switch(action.type){
-        case 'ADD':
-            return [action.blog,...state];
-        case 'Delete':
-            return state.filter((blog,index)=> index!==action.index);
-        default:
-            return [];        
-    }
-}
+// function blogsReducer(state,action){
+//     switch(action.type){
+//         case 'ADD':
+//             return [action.blog,...state];
+//         case 'Delete':
+//             return state.filter((blog,index)=> index!==action.index);
+//         default:
+//             return [];        
+//     }
+// }
 
 export default function Blog() {
 
     // const[title,setTitle]=useState('');
     // const[content,setContent]=useState('');
-    const[formData,setFormData]=useState({title:'',content:''})
-    // const[blogs,setBlogs]=useState([]);
+    const [formData, setFormData] = useState({ title: '', content: '' })
+    const [blogs, setBlogs] = useState([]);
     // const[editBlogs,setEditBlogs]=useState({title:'',content:''});
-    const[blogs,dispatch]=useReducer(blogsReducer,[]);
+    const [search, setSearch] = useState('');
+    // const[blogs,dispatch]=useReducer(blogsReducer,[]);
     const titleRef = useRef(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         titleRef.current.focus()
-    },[]);
+    }, []);
 
-    useEffect(()=>{
-        if(blogs.length && blogs[0].title){
+    useEffect(() => {
+        if (blogs.length && blogs[0].title) {
             document.title = blogs[0].title;
-        }else{
+        } else {
             document.title = 'No Blogs!'
         }
-    },[blogs]);
+    }, [blogs]);
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        
-        // setBlogs([{title:formData.title,content:formData.content},...blogs]);
-        dispatch({type:'ADD',blog:{title:formData.title,content:formData.content}});
+
+        setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+        // dispatch({type:'ADD',blog:{title:formData.title,content:formData.content}});
         // setTitle('');
         // setContent('');
-        setFormData({title:'',content:''});
+        setFormData({ title: '', content: '' });
         titleRef.current.focus();
     }
-    const handleDelete = (index)=>{
-        // let restBlog = blogs.filter((blog,i)=>i!==index);
-        // setBlogs(restBlog);
-        dispatch({type:'Delete',index:index})
+    const handleDelete = (index) => {
+        let restBlog = blogs.filter((blog, i) => i !== index);
+        setBlogs(restBlog);
+        // dispatch({type:'Delete',index:index})
     }
-    const handleEdit = (index)=>{
-        
+    const handleEdit = (index) => {
+        // Get the blog at the specified index
+        const blogToEdit = blogs[index];
+        // Set the formData state with the values of the blog to populate the form
+        setFormData({ title: blogToEdit.title, content: blogToEdit.content });
+        // Remove the blog from the blogs array
+        const updatedBlogs = [...blogs];
+        updatedBlogs.splice(index, 1);
+        setBlogs(updatedBlogs);
     }
-    
+    const filteredBlogs = blogs.filter((blog) =>
+        blog.title.toLowerCase().includes(search.toLowerCase())
+    );
+
     return (
         <>
-        <h1>Write A Blog!</h1>
-        <div className='section'>
-            <form onSubmit={handleSubmit}>
-                {/* Row component to create a row for first input field */}
-                <Row label='Title'>
-                    <input className='input' placeholder="Enter the Title of the Blog here.." value={formData.title} ref={titleRef} onChange={(e)=>setFormData({title:e.target.value,content:formData.content})}/>
-                </Row>
-                {/* Row component to create a row for Text area field */}
-                <Row label='Content'>
-                    <textarea className='input content' placeholder="Content of the Blog goes here.." required value={formData.content} onChange={(e)=>setFormData({title:formData.title,content:e.target.value})}/>
-                </Row>
-                <button className = "btn">ADD</button>
-            </form>
-        </div>
-        <hr />
-        <h2>Blogs</h2>
-        {blogs.map((blog,index)=>(
-            <div className='blog' key={index}>
-                <h3>{blog.title}</h3>
-                <p>{blog.content}</p>
-                <div className='blog-btn'>
-                    <button className='btn edit' onClick={()=>handleEdit(index)}>Edit</button>
-                    <button className='btn remove' onClick={()=>handleDelete(index)}>Delete</button>
-                </div>
+            <h1>Write A Blog!</h1>
+            <div className="search">
+                <input className="input" type="text" placeholder="Search from wishList...." onChange={(e) => setSearch(e.target.value)}/>
             </div>
-        ))}
+            <div className='section'>
+                <form onSubmit={handleSubmit}>
+                    {/* Row component to create a row for first input field */}
+                    <Row label='Title'>
+                        <input className='input' placeholder="Enter the Title of the Blog here.." value={formData.title} ref={titleRef} onChange={(e) => setFormData({ title: e.target.value, content: formData.content })} />
+                    </Row>
+                    {/* Row component to create a row for Text area field */}
+                    <Row label='Content'>
+                        <textarea className='input content' placeholder="Content of the Blog goes here.." required value={formData.content} onChange={(e) => setFormData({ title: formData.title, content: e.target.value })} />
+                    </Row>
+                    <button className="btn">ADD</button>
+                </form>
+            </div>
+            <hr />
+            <h2>Blogs</h2>
+            {filteredBlogs.map((blog, index) => (
+                <div className='blog' key={index}>
+                    <h3>{blog.title}</h3>
+                    <p>{blog.content}</p>
+                    <div className='blog-btn'>
+                        <button className='btn edit' onClick={() => handleEdit(index)}>Edit</button>
+                        <button className='btn remove' onClick={() => handleDelete(index)}>Delete</button>
+                    </div>
+                </div>
+            ))}
         </>
     )
 }
 
 //Row component to introduce a new row section in the form
-function Row(props){
-    const{label} = props;
-    return(
+function Row(props) {
+    const { label } = props;
+    return (
         <>
-        <label>{label}<br/></label>
-        {props.children}
-        <hr />
+            <label>{label}<br /></label>
+            {props.children}
+            <hr />
         </>
     )
 }
